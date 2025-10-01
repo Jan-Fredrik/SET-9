f36e76543ae60881f9aacb0fa6bd207b30df32c2
rxmate1 <Marius.w.larsen2@gmail.com>
2025-10-01 17:52:45 +0200

    ..
    ..

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,4 +4,5 @@
 public class main {
     String commit = "Jan Fredrik er ekstremt kjekk";
     String takk_marius = "Takk marius, du er snill å god.";
+    String bare_hyggerlig = "Bare hyggerlig, du er best";
 }
