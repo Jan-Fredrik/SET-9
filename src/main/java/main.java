41841b7bcc8b5fd718ce1f83776171ee9051a01e
Jan-Fredrik <jholsbrekken@gmail.com>
2025-10-13 19:37:27 +0200

    la til noen mapper

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,4 +5,5 @@ public class main {
     String commit = "Jan Fredrik er ekstremt kjekk";
     String takk_marius = "Takk marius, du er snill å god.";
     String bare_hyggerlig = "Bare hyggerlig, du er best";
+    String sjekk = "JF sjekk";
 }
